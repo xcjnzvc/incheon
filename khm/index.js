964844fb5a4d4b33ae1a62336b6d8bcdc35207c6
Kimhyunmook <gusanr4200@naver.com>
2024-09-26 17:32:47 +0900

    last fix

--- khm/index.js
+++ khm/index.js
@@ -1,5 +1,6 @@
 const label = document.querySelectorAll(".label");
 // const content = document.querySelectorAll(".content");
+let triger = true;
 
 label.forEach(async (el, i) => {
   const p_h = el.parentNode.clientHeight + "px";
@@ -9,6 +10,10 @@ label.forEach(async (el, i) => {
     const h = target.children[1].clientHeight + "px";
     target.children[1].style.height = 0;
     el.addEventListener("click", async (e) => {
+      if (!triger) {
+        alert("글자가 전부 출력 되기 전엔 클릭이 안되요!");
+        return;
+      }
       if (target.classList.value.includes("on")) {
         target.classList.remove("on");
         el.parentNode.children[1].style.height = 0;
@@ -25,15 +30,20 @@ label.forEach(async (el, i) => {
           let result = "";
           const stream = (text) =>
             new Promise((resolve, reject) => {
-              if (!target.classList.value.includes("on")) reject();
-              setTimeout(() => {
-                result += text;
-                stremTarget.innerHTML = result;
-                resolve();
-              }, 100);
+              try {
+                setTimeout(() => {
+                  result += text;
+                  stremTarget.innerHTML = result;
+                  resolve();
+                }, 50);
+              } catch (err) {
+                console.log(err);
+              }
             });
-          for (let text of streamText) {
-            await stream(text);
+          for (let i = 0; i < streamText.length; i++) {
+            triger = false;
+            await stream(streamText[i]);
+            if (i === streamText.length - 1) triger = true;
           }
         }
       }
@@ -49,6 +59,30 @@ label.forEach(async (el, i) => {
 
 const inputs = document.querySelectorAll("input");
 
-inputs.map((el) => {
-  
+[...inputs].map((el) => {
+  // console.log(el);
+  el.addEventListener("keyup", (e) => {
+    console.log(e.target.value.length);
+    if (e.target.value.length > 10) el.style.backgroundColor = "skyblue";
+    else el.style.backgroundColor = "#fff";
+  });
+});
+
+const myMind = document.querySelector(".myMind");
+myMind.addEventListener("mouseover", () => {
+  myMind.style.backgroundColor = "red";
+  myMind.innerHTML = "😠";
+});
+myMind.addEventListener("mouseout", () => {
+  myMind.style.backgroundColor = "blue";
+  myMind.innerHTML = "😭";
+});
+
+const el = document.createElement("div");
+el.innerHTML = "New Div";
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY > 30) {
+    el.classList.add("newDiv");
+    document.body.append(el);
+  }
 });
